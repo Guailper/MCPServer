b6ef9bb9c2c4f577a29b0a194d94564a488c4185
Guailper <3031975593@qq.com>
2026-05-13 22:38:07 +0800

    weather mcp update

--- weather_mcp/weather.py
+++ weather_mcp/weather.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 import httpx
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
@@ -35,7 +35,7 @@ def _get_location_id(location: str) -> str:
 def get_now_weather(location: str) -> Any:
     """
     获取当前天气
-    location: 城市名称
+        - location: 城市名称
     """
     # 获取地理位置ID
     location_id = _get_location_id(location)
@@ -52,3 +52,59 @@ def get_now_weather(location: str) -> Any:
         return {"error": "无法获取天气数据"}
     
     return weather_data["now"]
+
+@mcp.tool()
+def get_3d_forecast_weather(location: str, offset: Optional[int] = None) -> Any:
+    """
+    获取未来三天的天气预报
+        - location: 城市名称
+        - offset: 偏移量, 用于获取未来第N天的天气预报, 0表示今天, 1表示明天, 2表示后天
+    """
+    # 获取地理位置ID
+    location_id = _get_location_id(location)
+    
+    # 获取天气预报
+    weather_params = {
+        "location": location_id,
+        "key": QWEATHER_API_KEY
+    }
+    weather_response = httpx.get(QWEATHER_API_BASE + "3d", params=weather_params, headers={"User-Agent": USER_AGENT})
+    weather_data = weather_response.json()
+    
+    if weather_data.get("code") != "200":
+        return {"error": "无法获取天气数据"}
+    
+    if offset is not None:
+        if offset < 0 or offset > 2:
+            return {"error": "偏移量必须在0到2之间"}
+        return weather_data["daily"][offset]
+    
+    return weather_data["daily"]
+
+@mcp.tool()
+def get_7d_forecast_weather(location: str, offset: Optional[int] = None) -> Any:
+    """
+    获取未来七天的天气预报
+        - location: 城市名称
+        - offset: 偏移量, 用于获取未来第N天的天气预报, 0表示今天, 1表示明天, 以此类推，直到6表示未来第七天
+    """
+    # 获取地理位置ID
+    location_id = _get_location_id(location)
+    
+    # 获取天气预报
+    weather_params = {
+        "location": location_id,
+        "key": QWEATHER_API_KEY
+    }
+    weather_response = httpx.get(QWEATHER_API_BASE + "7d", params=weather_params, headers={"User-Agent": USER_AGENT})
+    weather_data = weather_response.json()
+    
+    if weather_data.get("code") != "200":
+        return {"error": "无法获取天气数据"}
+    
+    if offset is not None:
+        if offset < 0 or offset > 6:
+            return {"error": "偏移量必须在0到6之间"}
+        return weather_data["daily"][offset]
+    
+    return weather_data["daily"]
